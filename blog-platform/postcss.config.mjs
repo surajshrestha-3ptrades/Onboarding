8fac56e679f8fe4fe39f4e936ff660c097756016
Sonu Keshari <60221166+sonukeshari@users.noreply.github.com>
2026-01-30 10:53:49 +0545

    fixes error occurs with postcss.configt.mjs

--- blog-platform/postcss.config.mjs
+++ blog-platform/postcss.config.mjs
@@ -1,5 +1,8 @@
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // Use this instead of 'tailwindcss'
+    '@tailwindcss/postcss': {},
   },
-}
+};
+
+export default config;
